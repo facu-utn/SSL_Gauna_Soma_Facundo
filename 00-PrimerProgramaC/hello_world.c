e09b3ec910ce357d2e51bf0f53e792de9b17390f
Facundo Gauna Somá <fgaunasoma@frba.utn.edu.ar>
2023-04-18 01:23:16 -0300

    Creé mi primer archivo C: hello_world.c

---
+++ 00-PrimerProgramaC/hello_world.c
@@ -0,0 +1,16 @@
+#include <stdio.h> //.h stands for header file
+int main ()
+    {
+        printf  ("Hello Dear World!\n");
+        return 0;        
+    }
+
+//Para compilar escribo en terminal gcc nombreDelArchivo.c y se crea el ejecutable.
+/*Si deseo el ejecutable con otro nombre uso el comando -o asi:
+gcc nombreDelArchivo.c -o nombreDelEjecutable.exe 😁  */
+
+//Para ejecutar ./nombreDeEjecutable.exe
+
+//A diferencia de C++, C utiliza la función print f (), instead of cout <<
+
+//To insert a new line, you can use the \n character
